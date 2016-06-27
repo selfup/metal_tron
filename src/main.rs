167bc0e129049c8435dd63acb4cac3a746642f80
Regis <boudinot.regis@yahoo.com>
2016-06-26 20:25:07 -0600

    working lil game

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
+#[allow(unused_variables)]
 fn main() {
-    let mut bike_one = Bike::init();
+    let grid = Grid::new();
 
+    let mut bike_one = Bike::new();
+    
+    bike_one.print_properties();
+    bike_one.move_right();
     bike_one.print_properties();
 
-    bike_one.move_right();
+    for i in 0..800 {
+        bike_one.x += 1;
+    }
 
+    bike_one.status_check(grid);
     bike_one.print_properties();
 }
 
 struct Grid {
-    
+    x_limit: i16,
+    y_limit: i16,
+}
+
+impl Grid {
+    fn new() -> Grid {
+        Grid {
+            x_limit: 800,
+            y_limit: 800,
+        }
+    }
 }
 
 struct Bike {
-    x: i32,
-    y: i32,
+    x: i16,
+    y: i16,
     status: &'static str,
 }
 
 impl Bike {
-    fn init() -> Bike {
+    fn new() -> Bike {
         Bike {
             x: 0,
             y: 0,
@@ -39,6 +57,10 @@ impl Bike {
     }
 
     fn status_check(&mut self, grid: Grid) {
-
+        if self.x >= grid.x_limit {
+            self.x = 0;
+        } else if self.y >= grid.y_limit {
+            self.y = 0;
+        }
     }
 }
