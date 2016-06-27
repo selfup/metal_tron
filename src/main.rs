3d368b30fc0e8e47b0e3e868c67ae44137114ee5
Regis <boudinot.regis@yahoo.com>
2016-06-27 11:27:55 -0600

    updated game to handle player facing logic

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,55 @@
 #[allow(unused_variables)]
 fn main() {
-    let mut game = Game::new(Grid::new(800, 800), Bike::new(0, 0));
+    let mut game = Game::new(Grid::new(800, 0, 800, 0),
+                             Bike::new(0, 400, "bike_one".to_string()),
+                             Bike::new(800, 400, "bike_two".to_string()));
 
-    game.bike_one.move_right();
-    game.bike_one.status_check(&game.grid);
+    game.bike_one_right();
+    game.bike_one_left();
     game.bike_one.print_properties();
 
-    for i in 0..802 {
-        game.bike_one.move_right();
-        game.bike_one.status_check(&game.grid);
-    }
-
-    game.bike_one.move_left();
-    game.bike_one.status_check(&game.grid);
-    game.bike_one.print_properties();
+    game.bike_two_right();
+    game.bike_two_left();
+    game.bike_two.print_properties();
 }
 
 struct Game {
     grid: Grid,
     bike_one: Bike,
+    bike_two: Bike,
 }
 
 impl Game {
-    fn new(grid: Grid, bike_one: Bike) -> Game {
+    fn new(grid: Grid, bike_one: Bike, bike_two: Bike) -> Game {
         Game {
             grid: grid,
             bike_one: bike_one,
+            bike_two: bike_two,
         }
+    }
+
+    fn bike_one_right(&mut self) {
+        self.bike_one.move_right();
+        self.bike_one.grid_check(&self.grid);
+        self.grid.collect_bike_one_trail(&self.bike_one);
+    }
+
+    fn bike_one_left(&mut self) {
+        self.bike_one.move_left();
+        self.bike_one.grid_check(&self.grid);
+        self.grid.collect_bike_one_trail(&self.bike_one);
+    }
+
+    fn bike_two_right(&mut self) {
+        self.bike_one.move_right();
+        self.bike_one.grid_check(&self.grid);
+        self.grid.collect_bike_one_trail(&self.bike_one);
+    }
+
+    fn bike_two_left(&mut self) {
+        self.bike_one.move_left();
+        self.bike_one.grid_check(&self.grid);
+        self.grid.collect_bike_one_trail(&self.bike_one);
     }
 }
 
@@ -35,39 +58,53 @@ struct Grid {
     x_min: i16,
     y_max: i16,
     y_min: i16,
+    trails: Vec<(i16, i16)>,
 }
 
 impl Grid {
-    fn new(x_max: i16, y_max: i16) -> Grid {
+    fn new(x_max: i16, x_min: i16, y_max: i16, y_min: i16) -> Grid {
+        let mut initial_trails = Vec::new();
+
+        initial_trails.push((x_min, y_max / 2));
+        initial_trails.push((x_max, y_max / 2));
+
         Grid {
             x_max: x_max,
-            x_min: 0,
+            x_min: x_min,
             y_max: y_max,
-            y_min: 0,
+            y_min: y_min,
+            trails: initial_trails,
         }
+    }
+
+    fn collect_bike_one_trail(&mut self, bike_one: &Bike) {
+        self.trails.push((bike_one.x, bike_one.y));
     }
 }
 
 struct Bike {
     x: i16,
     y: i16,
-    status: &'static str,
+    name: String,
+    status: String,
 }
 
 impl Bike {
-    fn new(x: i16, y: i16) -> Bike {
+    fn new(x: i16, y: i16, name: String) -> Bike {
         Bike {
             x: x,
             y: y,
-            status: "alive",
+            name: name,
+            status: "alive".to_string(),
         }
     }
 
     fn print_properties(&self) {
-        println!("Bike coords: x -> {}, y -> {}, Bike status: {}",
+        println!("{name} coords: x -> {}, y -> {}, {name} status: {}",
                  self.x,
                  self.y,
-                 self.status);
+                 self.status,
+                 name = self.name);
     }
 
     fn move_right(&mut self) {
@@ -78,7 +115,7 @@ impl Bike {
         self.x -= 1;
     }
 
-    fn status_check(&mut self, grid: &Grid) {
+    fn grid_check(&mut self, grid: &Grid) {
         if self.x > grid.x_max {
             self.x = grid.x_min;
         } else if self.y > grid.y_max {
