8a6575516019b4f5265c6435b32440b37013aa43
Regis <boudinot.regis@yahoo.com>
2016-06-27 00:19:55 -0600

    boundary checks complete

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
 #[allow(unused_variables)]
 fn main() {
     let grid = Grid::new();
-
     let mut bike_one = Bike::new();
-    
+
     bike_one.print_properties();
     bike_one.move_right();
-    bike_one.print_properties();
+    bike_one.status_check(&grid);
 
-    for i in 0..800 {
-        bike_one.x += 1;
+    for i in 0..802 {
+        bike_one.move_right();
+        bike_one.status_check(&grid);
     }
 
-    bike_one.status_check(grid);
+    bike_one.move_left();
+    bike_one.status_check(&grid);
     bike_one.print_properties();
 }
 
 struct Grid {
-    x_limit: i16,
-    y_limit: i16,
+    x_max: i16,
+    x_min: i16,
+    y_max: i16,
+    y_min: i16,
 }
 
 impl Grid {
     fn new() -> Grid {
         Grid {
-            x_limit: 800,
-            y_limit: 800,
+            x_max: 800,
+            x_min: 0,
+            y_max: 800,
+            y_min: 0,
         }
     }
 }
@@ -56,11 +61,19 @@ impl Bike {
         self.x += 1;
     }
 
-    fn status_check(&mut self, grid: Grid) {
-        if self.x >= grid.x_limit {
-            self.x = 0;
-        } else if self.y >= grid.y_limit {
-            self.y = 0;
+    fn move_left(&mut self) {
+        self.x -= 1;
+    }
+
+    fn status_check(&mut self, grid: &Grid) {
+        if self.x > grid.x_max {
+            self.x = grid.x_min;
+        } else if self.y > grid.y_max {
+            self.y = grid.y_min;
+        } else if self.x < grid.x_min {
+            self.x = grid.x_max;
+        } else if self.y < grid.y_min {
+            self.y = grid.y_max;
         }
     }
 }
