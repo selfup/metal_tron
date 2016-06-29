e8ed1a23cccc484a37f012ca7c3eaad44571ec5e
Regis <boudinot.regis@yahoo.com>
2016-06-29 17:56:27 -0600

    ensuring it can run quickly

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@ fn main() {
                              Bike::new(0, 400),
                              Bike::new(800, 400));
 
-    game.bike_one_right();
-    game.bike_one_left();
-    game.bike_one_up();
-    game.bike_one_down();
+    for _ in 0..1001 {
+        game.bike_one_right();
+        game.bike_one_left();
+        game.bike_one_up();
+        game.bike_one_down();
 
-    game.bike_two_left();
-    game.bike_two_right();
-    game.bike_two_up();
-    game.bike_two_down();
+        game.bike_two_left();
+        game.bike_two_right();
+        game.bike_two_up();
+        game.bike_two_down();
+    }
 }
