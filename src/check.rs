ced8e5b3b03d4198b254a3ffa0be8478428ddca1
Regis <boudinot.regis@yahoo.com>
2016-06-29 16:05:47 -0600

    no need for mut args

--- src/check.rs
+++ src/check.rs
@@ -1,7 +1,7 @@
 use grid::*;
 use bike::*;
 
-pub fn all_checks(mut grid: &mut Grid, mut bike: &mut Bike) {
+pub fn all_checks(grid: &mut Grid, bike: &mut Bike) {
     bike.grid_check(grid);
     grid.collect_bike_trail(bike);
     bike.alive_or_not(grid);
